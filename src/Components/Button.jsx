5edfcd13e737995f9f911c79e0730ecdaf03ec34
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-29 00:09:12 +1100

    add transition

--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Button = ({ text, color, doSomething, disabled }) => {
   return (
     <button
-      className="px-5 text-white text-xl font-medium py-3 rounded-full shadow-lg"
+      className="px-5 text-white text-xl font-medium py-3 rounded-full shadow-lg transition ease-in-out hover:-translate-y-1 hover:scale-110 duration-200"
       style={{ backgroundColor: color }}
       onClick={doSomething}
       disabled={disabled}
