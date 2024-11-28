8d3119e18b5311897d00af7c09ab16a4d8f46c9c
Prashrijan <shresthaprashrijan@gmail.com>
2024-11-29 00:01:52 +1100

    react counter app ready

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,28 @@ import { useState } from "react";
 import Button from "./Components/Button";
 
 const App = () => {
+  const handleIncrease = () => {
+    setCount((prevCount) => prevCount + 1);
+  };
+  const handleReset = () => {
+    setCount(0);
+  };
+  const handleDecrease = () => {
+    setCount((prevCount) => prevCount - 1);
+  };
   let [count, setCount] = useState(0);
   return (
     <div className="wrapper h-screen w-full bg-slate-800 flex flex-col items-center justify-center">
       <h1 className="text-white text-4xl font-semibold ">Count: {count}</h1>
       <div className="buttons flex gap-3 mt-6">
-        <Button text="Increase" color="green" />
-        <Button text="Reset" color="black" />
-        <Button text="Decrease" color="red" />
+        <Button text="Increase" color="green" doSomething={handleIncrease} />
+        <Button text="Reset" color="black" doSomething={handleReset} />
+        <Button
+          text="Decrease"
+          color="red"
+          doSomething={handleDecrease}
+          disabled={count === 0}
+        />
       </div>
     </div>
   );
--- src/Components/Button.jsx
+++ src/Components/Button.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
-const Button = ({ text, color }) => {
+const Button = ({ text, color, doSomething, disabled }) => {
   return (
     <button
       className="px-5 text-white text-xl font-medium py-3 rounded-full shadow-lg"
       style={{ backgroundColor: color }}
+      onClick={doSomething}
+      disabled={disabled}
     >
       {text}
     </button>
